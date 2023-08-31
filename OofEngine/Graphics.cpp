5d60541d581a59b7cb9d9aca67846a8908806295
alswang18 <alec.sy.wang@gmail.com>
2023-08-31 00:47:36 -0700

    ran format

--- OofEngine/Graphics.cpp
+++ OofEngine/Graphics.cpp
@@ -44,89 +44,87 @@ namespace wrl = Microsoft::WRL;
 #define GFX_THROW_INFO_ONLY(call) (call)
 #endif
 
-Graphics::Graphics( HWND hWnd )
+Graphics::Graphics(HWND hWnd)
 {
-	DXGI_SWAP_CHAIN_DESC sd = {};
-	sd.BufferDesc.Width = 0;
-	sd.BufferDesc.Height = 0;
-	sd.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
-	sd.BufferDesc.RefreshRate.Numerator = 0;
-	sd.BufferDesc.RefreshRate.Denominator = 0;
-	sd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
-	sd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
-	sd.SampleDesc.Count = 1;
-	sd.SampleDesc.Quality = 0;
-	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-	sd.BufferCount = 1;
-	sd.OutputWindow = hWnd;
-	sd.Windowed = TRUE;
-	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
-	sd.Flags = 0;
+  DXGI_SWAP_CHAIN_DESC sd = {};
+  sd.BufferDesc.Width = 0;
+  sd.BufferDesc.Height = 0;
+  sd.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+  sd.BufferDesc.RefreshRate.Numerator = 0;
+  sd.BufferDesc.RefreshRate.Denominator = 0;
+  sd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+  sd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+  sd.SampleDesc.Count = 1;
+  sd.SampleDesc.Quality = 0;
+  sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+  sd.BufferCount = 1;
+  sd.OutputWindow = hWnd;
+  sd.Windowed = TRUE;
+  sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+  sd.Flags = 0;
 
-	UINT swapCreateFlags = 0u;
+  UINT swapCreateFlags = 0u;
 #ifndef NDEBUG
-	swapCreateFlags |= D3D11_CREATE_DEVICE_DEBUG;
+  swapCreateFlags |= D3D11_CREATE_DEVICE_DEBUG;
 #endif
 
-	// for checking results of d3d functions
-	HRESULT hr;
+  // for checking results of d3d functions
+  HRESULT hr;
 
-	// create device and front/back buffers, and swap chain and rendering context
-	GFX_THROW_INFO( D3D11CreateDeviceAndSwapChain(
-		nullptr,
-		D3D_DRIVER_TYPE_HARDWARE,
-		nullptr,
-		swapCreateFlags,
-		nullptr,
-		0,
-		D3D11_SDK_VERSION,
-		&sd,
-		&pSwap,
-		&pDevice,
-		nullptr,
-		&pContext
-	) );
-	// gain access to texture subresource in swap chain (back buffer)
-	wrl::ComPtr<ID3D11Resource> pBackBuffer;
-	GFX_THROW_INFO( pSwap->GetBuffer( 0,__uuidof(ID3D11Resource),&pBackBuffer ) );
-	GFX_THROW_INFO( pDevice->CreateRenderTargetView( pBackBuffer.Get(),nullptr,&pTarget ) );
-	
-	// create depth stensil state
-	D3D11_DEPTH_STENCIL_DESC dsDesc = {};
-	dsDesc.DepthEnable = TRUE;
-	dsDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
-	dsDesc.DepthFunc = D3D11_COMPARISON_LESS;
-	wrl::ComPtr<ID3D11DepthStencilState> pDSState;
-	GFX_THROW_INFO( pDevice->CreateDepthStencilState( &dsDesc,&pDSState ) );
+  // create device and front/back buffers, and swap chain and rendering context
+  GFX_THROW_INFO(D3D11CreateDeviceAndSwapChain(nullptr,
+                                               D3D_DRIVER_TYPE_HARDWARE,
+                                               nullptr,
+                                               swapCreateFlags,
+                                               nullptr,
+                                               0,
+                                               D3D11_SDK_VERSION,
+                                               &sd,
+                                               &pSwap,
+                                               &pDevice,
+                                               nullptr,
+                                               &pContext));
+  // gain access to texture subresource in swap chain (back buffer)
+  wrl::ComPtr<ID3D11Resource> pBackBuffer;
+  GFX_THROW_INFO(pSwap->GetBuffer(0, __uuidof(ID3D11Resource), &pBackBuffer));
+  GFX_THROW_INFO(
+    pDevice->CreateRenderTargetView(pBackBuffer.Get(), nullptr, &pTarget));
 
-	// bind depth state
-	pContext->OMSetDepthStencilState( pDSState.Get(),1u );
+  // create depth stensil state
+  D3D11_DEPTH_STENCIL_DESC dsDesc = {};
+  dsDesc.DepthEnable = TRUE;
+  dsDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
+  dsDesc.DepthFunc = D3D11_COMPARISON_LESS;
+  wrl::ComPtr<ID3D11DepthStencilState> pDSState;
+  GFX_THROW_INFO(pDevice->CreateDepthStencilState(&dsDesc, &pDSState));
 
-	// create depth stensil texture
-	wrl::ComPtr<ID3D11Texture2D> pDepthStencil;
-	D3D11_TEXTURE2D_DESC descDepth = {};
-	descDepth.Width = 800u;
-	descDepth.Height = 600u;
-	descDepth.MipLevels = 1u;
-	descDepth.ArraySize = 1u;
-	descDepth.Format = DXGI_FORMAT_D32_FLOAT;
-	descDepth.SampleDesc.Count = 1u;
-	descDepth.SampleDesc.Quality = 0u;
-	descDepth.Usage = D3D11_USAGE_DEFAULT;
-	descDepth.BindFlags = D3D11_BIND_DEPTH_STENCIL;
-	GFX_THROW_INFO( pDevice->CreateTexture2D( &descDepth,nullptr,&pDepthStencil ) );
+  // bind depth state
+  pContext->OMSetDepthStencilState(pDSState.Get(), 1u);
 
-	// create view of depth stensil texture
-	D3D11_DEPTH_STENCIL_VIEW_DESC descDSV = {};
-	descDSV.Format = DXGI_FORMAT_D32_FLOAT;
-	descDSV.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
-	descDSV.Texture2D.MipSlice = 0u;
-	GFX_THROW_INFO( pDevice->CreateDepthStencilView(
-		pDepthStencil.Get(),&descDSV,&pDSV
-	) );
+  // create depth stensil texture
+  wrl::ComPtr<ID3D11Texture2D> pDepthStencil;
+  D3D11_TEXTURE2D_DESC descDepth = {};
+  descDepth.Width = 800u;
+  descDepth.Height = 600u;
+  descDepth.MipLevels = 1u;
+  descDepth.ArraySize = 1u;
+  descDepth.Format = DXGI_FORMAT_D32_FLOAT;
+  descDepth.SampleDesc.Count = 1u;
+  descDepth.SampleDesc.Quality = 0u;
+  descDepth.Usage = D3D11_USAGE_DEFAULT;
+  descDepth.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+  GFX_THROW_INFO(pDevice->CreateTexture2D(&descDepth, nullptr, &pDepthStencil));
 
-	// bind depth stensil view to OM
-	pContext->OMSetRenderTargets( 1u,pTarget.GetAddressOf(),pDSV.Get() );
+  // create view of depth stensil texture
+  D3D11_DEPTH_STENCIL_VIEW_DESC descDSV = {};
+  descDSV.Format = DXGI_FORMAT_D32_FLOAT;
+  descDSV.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+  descDSV.Texture2D.MipSlice = 0u;
+  GFX_THROW_INFO(
+    pDevice->CreateDepthStencilView(pDepthStencil.Get(), &descDSV, &pDSV));
+
+  // bind depth stensil view to OM
+  pContext->OMSetRenderTargets(1u, pTarget.GetAddressOf(), pDSV.Get());
 }
 
 void
@@ -145,11 +143,12 @@ Graphics::EndFrame()
   }
 }
 
-void Graphics::ClearBuffer( float red,float green,float blue ) noexcept
+void
+Graphics::ClearBuffer(float red, float green, float blue) noexcept
 {
-	const float color[] = { red,green,blue,1.0f };
-	pContext->ClearRenderTargetView( pTarget.Get(),color );
-	pContext->ClearDepthStencilView( pDSV.Get(),D3D11_CLEAR_DEPTH,1.0f,0u );
+  const float color[] = { red, green, blue, 1.0f };
+  pContext->ClearRenderTargetView(pTarget.Get(), color);
+  pContext->ClearDepthStencilView(pDSV.Get(), D3D11_CLEAR_DEPTH, 1.0f, 0u);
 }
 
 void
